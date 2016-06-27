52a06a4ff3d093f92f59e481c138d4dec8faf9a4
Jeni Rotaru <rotaru.jeni01@gmail.com>
2016-06-27 17:53:49 +0300

    Day 3

--- Day3/scripts/controllers/MenuController.js
+++ Day3/scripts/controllers/MenuController.js
@@ -1,10 +1,6 @@
 hrApp.controller('MenuController', ['$scope', 'employeeActionsService', function($scope, employeeActionsService){
-    $scope.employeeActionList = [{
-        url:'#/employeeslist',
-        label:'Employee List'
-    }];
-
     // TODO #12 - load menu items from Value
+    $scope.employeeActionList = employeeActionsService;
 
     $scope.currentDate = new Date();
 
--- Day3/scripts/controllers/demo/RequestController.js
+++ Day3/scripts/controllers/demo/RequestController.js
@@ -1,8 +1,14 @@
-hrApp.controller('RequestController', ['$scope', function($scope){
+hrApp.controller('RequestController', ['$scope', '$http', function($scope, $http){
 
     $scope.jobList = [];
 
 //    TODO #10 - make AJAX call
-
+    $http({url:"http://hrapp-zth.rhcloud.com/hrapp/jobs", method:"GET"})
+        .success(function(data, status, headers, config) {
+            $scope.jobList = data;
+        })
+        .error (function(data, status, headers, config){
+            alert("Error: " + status);
+        });
 
 }]);
